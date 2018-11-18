28addab76bd4af0a131e2cc84783ca4a6bcd91df
Ozgur Oktay <ozgur@snapbytes.com>
2018-11-18 14:20:41 +0300

    vs2017 changes

--- tools/keys/gennetkey.cmd
+++ tools/keys/gennetkey.cmd
@@ -1,2 +1,2 @@
-if not exist "%~dp0/jni4net.snk" "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k "%~dp0/jni4net.snk"
-if not exist "%~dp0/jni4net.snk" "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k "%~dp0/jni4net.snk"
+if not exist "%~dp0\jni4net.snk" "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" -k "%~dp0/jni4net.snk"
+if not exist "%~dp0\jni4net.snk" "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe" -k "%~dp0/jni4net.snk"
